d2a72a2e16dcd5fd8d7fd4d8e51d68cebe5a363d
bipoza <bipoza@gmail.com>
2020-03-03 16:52:12 +0100

    adapting text with button

--- src/components/mastodon-share-button/mastodon-share-button.tsx
+++ src/components/mastodon-share-button/mastodon-share-button.tsx
@@ -76,7 +76,7 @@ export class MastodonShareButton {
 
         <button onClick={() => this.openModal()} class="share-button">
           {this.share_button_text}
-          <img src={this.icon_url} />
+          <img src={this.icon_url} class={this.share_button_text.length!=0?"icon-with-text":""}/>
         </button>
 
         <div class={'overlay ' + (this.open ? 'is-visible' : '') + ' ' + (this.dark_mode ? 'is-dark-mode' : '')} id="modal">
